5692aae5225ea8cbe9a98fb5ae050bb7580c12d7
sumit021995 <kaku.saurav@gmail.com>
2024-01-12 23:57:27 +0530

    updated file

--- CustomReact/CustomReact.js
+++ CustomReact/CustomReact.js
@@ -1,5 +1,7 @@
 function customRender(reactElement,container){
-    
+    const domElement = document.createElement
+    (reactElement.type)
+    domElement
 }
 
 const mainContainer = ReactDOM.createReact(document.getElementById('root'))
