10ae01a63f17c7683379f9f20690296d8343b5aa
sumit021995 <kaku.saurav@gmail.com>
2024-01-13 23:32:51 +0530

    file updated

--- CustomReact/CustomReact.js
+++ CustomReact/CustomReact.js
@@ -3,10 +3,11 @@
 const customRender = function (reactElement,container){
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children;
-    domElement.setAttribute("href",reactElement.props.href)
-    domElement.setAttribute("target",reactElement.props.target)
+    // domElement.setAttribute("href",reactElement.props.href)
+    // domElement.setAttribute("target",reactElement.props.target)
 
-    container.appendChild(domElement)
+    // container.appendChild(domElement)
+    
 }
 
 // A Simple not Standard React Element
