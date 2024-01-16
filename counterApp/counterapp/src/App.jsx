f04215f202bfde0b200056c13b5099dac32d6640
sumit021995 <kaku.saurav@gmail.com>
2024-01-16 16:03:28 +0530

    counter app vite-react

---
+++ counterApp/counterapp/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let count = 
+
+  return (
+    <>
+      <div>
+        <h1>Sumit , Vite-React , Counter App</h1>
+        <h2>Counter Value: {count}</h2>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
