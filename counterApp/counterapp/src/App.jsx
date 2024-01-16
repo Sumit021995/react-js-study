c2792a5c9ec318207da15488ebf67d854b8c6561
sumit021995 <kaku.saurav@gmail.com>
2024-01-16 21:37:36 +0530

    file updated

--- counterApp/counterapp/src/App.jsx
+++ counterApp/counterapp/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -21,11 +19,11 @@ function App() {
     <>
       <div>
         <h1>Sumit , Vite-React , Counter App</h1>
-        <h2>Counter Value: </h2>
+        
 
         <button onClick={addValue}>Add Value</button>
         <br/>
-        <h2>{count}</h2>
+        <h2>Counter Value: {count}</h2>
         <br/>
         <button onClick = {SubValue}>Subtract Value</button>
 
