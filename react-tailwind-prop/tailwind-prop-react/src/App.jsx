6a5f4d3c826fdb2cbf09ace14ab54b33a282fa11
sumit021995 <kaku.saurav@gmail.com>
2024-01-19 22:30:57 +0530

    file updated

--- react-tailwind-prop/tailwind-prop-react/src/App.jsx
+++ react-tailwind-prop/tailwind-prop-react/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-400 text-orange-400'>Tailwind Test</h1>
+      <h1 className='bg-green-400 text-orange-200'>Tailwind Test</h1>
     </>
   )
 }
