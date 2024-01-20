7a06d1156e044a0186364f30c531dc20e1b21a24
sumit021995 <kaku.saurav@gmail.com>
2024-01-20 23:52:14 +0530

    file updated

--- react-tailwind-prop/tailwind-prop-react/src/App.jsx
+++ react-tailwind-prop/tailwind-prop-react/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
 
 import './App.css'
+import './assets/sumit.jpeg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='bg-green-400 text-orange-200 p-4 m-4 rounded-xl' >Tailwind Test</h1>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img  src="./assets/sumit.jpeg" alt="Sumit" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
     </>
   )
 }
