efe53461d1b338b83db28fc497e7396d642e4597
sumit021995 <kaku.saurav@gmail.com>
2024-01-20 23:56:38 +0530

    file updated

--- react-tailwind-prop/tailwind-prop-react/src/App.jsx
+++ react-tailwind-prop/tailwind-prop-react/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <h1 className='bg-green-400 text-orange-200 p-4 m-4 rounded-xl' >Tailwind Test</h1>
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
   <img  src="./assets/sumit.jpeg" alt="Sumit" width="384" height="512"/>
+  <img src ='./assets/sumit.jpeg'/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
