9e104ddcffd0e7ccc4a96cb9456d9839779012e5
sumit021995 <kaku.saurav@gmail.com>
2024-01-21 22:17:28 +0530

    file updated

--- react-tailwind-prop/tailwind-prop-react/src/App.jsx
+++ react-tailwind-prop/tailwind-prop-react/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   />
   <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
   <div className="absolute bottom-4 left-4 text-left">
-    <h1 className="text-lg font-semibold text-white">Delba</h1>
+    <h1 className="text-lg font-semibold text-white">Sumit Saurav</h1>
     <p className="mt-2 text-sm text-gray-300">
     “A Full Stack Web Developer | A Passionate Web3 enthusiast | AlmaBetter Pro Member”
     </p>
