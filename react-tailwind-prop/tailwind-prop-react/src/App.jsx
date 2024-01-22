c0390d7bc30afc4b6093786b1fa9547b348f9033
sumit021995 <kaku.saurav@gmail.com>
2024-01-22 23:35:59 +0530

    file updated

--- react-tailwind-prop/tailwind-prop-react/src/App.jsx
+++ react-tailwind-prop/tailwind-prop-react/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import  sumit from './assets/sumit.jpeg'
+import  bhaiya from './assets/bhaiya.jpeg'
 import Card from './components/card'
 
 
@@ -8,9 +9,9 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-400 text-orange-200 p-4 m-4 rounded-xl'>Sumit Saurav</h1>
+      <h1 className='bg-green-400 text-orange-200 p-4 m-4 rounded-xl'>Profile Info</h1>
       <Card myImg={sumit}/>
-      <Card myImg={amit}/>
+      <Card myImg={bhaiya}/>
 
 
 
