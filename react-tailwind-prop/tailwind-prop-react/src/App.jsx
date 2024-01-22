88c421c0a6e7030a7b5eac0795232cb76ee9f8a2
sumit021995 <kaku.saurav@gmail.com>
2024-01-22 21:44:26 +0530

    file updated

--- react-tailwind-prop/tailwind-prop-react/src/App.jsx
+++ react-tailwind-prop/tailwind-prop-react/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-400 text-orange-200 p-4 m-4 rounded-xl' >Sumit Saurav</h1>
+      <h1 className='bg-green-400 text-orange-200 p-4 m-4 rounded-xl'>Sumit Saurav</h1>
       <Card/>
 
 
