81df54e0a5f1437b7236eb3111fbb4089b754528
sumit021995 <kaku.saurav@gmail.com>
2024-01-23 23:07:04 +0530

    file updated

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -15,17 +15,17 @@ function App() {
       <h1 className=' fixed flex flex-wrap justify-center bottom-4 inset-x-0 px-2 text-white' style ={{color:textColor}}>Click on Buttons to Change Color of Background</h1>
       <div className="flex flex-wrap px-3 py-2 bg-white justify-center gap-3 shadow-lg rounded-3xl">
       
-        <button onClick={ () => {return setColor("red"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1 text-white' style={{backgroundColor:'red'}}>Red</button>
-        <button onClick={ () => {return setColor("green"),setTextColor('white')}  } className='outline-none rounded-full justify-center px-4 py-1  text-white' style={{backgroundColor:'green'}}>Green</button>
-        <button onClick={ () => {return setColor("blue"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white' style={{backgroundColor:'blue'}}>Blue</button>
-        <button onClick={ () => {return setColor("gray"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white' style={{backgroundColor:'gray'}}>Gray</button>
-        <button onClick={ () => {return setColor("purple"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white' style={{backgroundColor:'purple'}}>Purple</button>
-        <button onClick={ () => {return setColor("pink"),setTextColor('black')} } className='outline-none rounded-full justify-center px-4 py-1  text-black' style={{backgroundColor:'pink'}}>Pink</button>
-        <button onClick={ () => {return setColor("olive"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white' style={{backgroundColor:'olive'}}>Olive</button>
-        <button onClick={ () => {return setColor("yellow"),setTextColor('black')} } className='outline-none rounded-full justify-center px-4 py-1  text-black' style={{backgroundColor:'yellow'}}>Yellow</button>
-        <button onClick={ () => {return setColor("black"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white' style={{backgroundColor:'black'}}>Black</button>
-        <button onClick={ () => {return setColor("white"),setTextColor('black')} } className='outline-none rounded-full justify-center px-4 py-1  text-black ' style={{backgroundColor:'white'}}>White</button>
-        <button onClick={ () => {return setColor("lavender"),setTextColor('black')} } className='outline-none rounded-full justify-center px-4 py-1  text-black' style={{backgroundColor:'lavender'}}>Lavender</button>
+        <button onClick={ () => {return setColor("red"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1 text-white shadow-lg shadow-slate-500' style={{backgroundColor:'red'}}>Red</button>
+        <button onClick={ () => {return setColor("green"),setTextColor('white')}  } className='outline-none rounded-full justify-center px-4 py-1  text-white shadow-lg shadow-slate-500' style={{backgroundColor:'green'}}>Green</button>
+        <button onClick={ () => {return setColor("blue"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white shadow-lg shadow-slate-500' style={{backgroundColor:'blue'}}>Blue</button>
+        <button onClick={ () => {return setColor("gray"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white shadow-lg shadow-slate-500' style={{backgroundColor:'gray'}}>Gray</button>
+        <button onClick={ () => {return setColor("purple"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white shadow-lg shadow-slate-500' style={{backgroundColor:'purple'}}>Purple</button>
+        <button onClick={ () => {return setColor("pink"),setTextColor('black')} } className='outline-none rounded-full justify-center px-4 py-1  text-black shadow-lg shadow-slate-500' style={{backgroundColor:'pink'}}>Pink</button>
+        <button onClick={ () => {return setColor("olive"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white shadow-lg shadow-slate-500' style={{backgroundColor:'olive'}}>Olive</button>
+        <button onClick={ () => {return setColor("yellow"),setTextColor('black')} } className='outline-none rounded-full justify-center px-4 py-1  text-black shadow-lg shadow-slate-500' style={{backgroundColor:'yellow'}}>Yellow</button>
+        <button onClick={ () => {return setColor("black"),setTextColor('white')} } className='outline-none rounded-full justify-center px-4 py-1  text-white shadow-lg shadow-slate-500' style={{backgroundColor:'black'}}>Black</button>
+        <button onClick={ () => {return setColor("white"),setTextColor('black')} } className='outline-none rounded-full justify-center px-4 py-1  text-black shadow-lg shadow-slate-500' style={{backgroundColor:'white'}}>White</button>
+        <button onClick={ () => {return setColor("lavender"),setTextColor('black')} } className='outline-none rounded-full justify-center px-4 py-1  text-black shadow-lg shadow-slate-500' style={{backgroundColor:'lavender'}}>Lavender</button>
       
       </div>
 
