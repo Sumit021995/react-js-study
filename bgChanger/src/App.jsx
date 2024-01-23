620ac09e686c4a602ba383a41413fe2b7aaf9973
sumit021995 <kaku.saurav@gmail.com>
2024-01-23 14:18:48 +0530

    file updated

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+
 
 function App() {
   const [color, setcolor] = useState("olive")
 
   return (
     
-      <div className = 'w-full h-screen duration-200 m-0 p-0 space-x-0 space-y-0' 
+      <div className = 'w-full h-screen duration-200' 
       style={{backgroundColor:color}}
       >
-      
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 '>
+
+      </div>
       </div>
     
   )
