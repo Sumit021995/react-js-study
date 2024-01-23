800f68d4bb59207ae2413df56aa329bc4eb1073b
sumit021995 <kaku.saurav@gmail.com>
2024-01-23 20:30:38 +0530

    file updated

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
       style={{backgroundColor:color}}
       >
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 '>
-      <div className="flex flex-wrap p-3 m-2 bg-white justify-center gap-3 shadow-lg rounded-3xl">
-        <button className='rounded-2xl justify-center'>Text</button>
+      <div className="flex flex-wrap p-3 m-2 bg-white justify-center gap-3 shadow-lg rounded-2xl">
+        <button className='rounded-3xl justify-center'>Text</button>
       
       </div>
 
