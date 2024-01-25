aded69cfe68349e1f60e48c64c70c3260b5d6803
sumit021995 <kaku.saurav@gmail.com>
2024-01-25 21:55:47 +0530

    file updated

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className=''>Password Generator</h1>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
       
     </>
   )
