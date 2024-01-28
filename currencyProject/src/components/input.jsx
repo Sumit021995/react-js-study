01b6587f35093e689e657bee5479664d7bbbd84c
sumit021995 <kaku.saurav@gmail.com>
2024-01-29 02:06:27 +0530

    file updated

--- currencyProject/src/components/input.jsx
+++ currencyProject/src/components/input.jsx
@@ -28,6 +28,7 @@ function Input(
                     placeholder="Amount"
                     disabled = {amountDisable}
                     value={amount}
+                    onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
