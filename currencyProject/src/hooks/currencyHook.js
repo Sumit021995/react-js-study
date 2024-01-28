cbae3b88b0e297e259d8a82b3ed416db70239150
sumit021995 <kaku.saurav@gmail.com>
2024-01-28 14:03:54 +0530

    file updated

--- currencyProject/src/hooks/currencyHook.js
+++ currencyProject/src/hooks/currencyHook.js
@@ -1,8 +1,11 @@
 import { useEffect , useState } from 'react'
 
 function useCurrencyHook (currency) {
+    const [data , setData] = useState({})
     useEffect(()=>{
-        fetch()
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then(()=>{})
 
     },[])
     return [currency , setCurrency]
