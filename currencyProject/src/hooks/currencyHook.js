841f2ab9866c06d8346a29a27233d9004bd4576f
sumit021995 <kaku.saurav@gmail.com>
2024-01-28 14:07:26 +0530

    File updated

--- currencyProject/src/hooks/currencyHook.js
+++ currencyProject/src/hooks/currencyHook.js
@@ -5,8 +5,8 @@ function useCurrencyHook (currency) {
     useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res)=>res.json())
-        .then(()=>{})
+        .then((res)=>setData(res[currency]))
 
     },[])
-    return [currency , setCurrency]
+
 }
