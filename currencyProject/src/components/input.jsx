3f6f808f02cb52db26785c60e8173c23c15b2e6a
sumit021995 <kaku.saurav@gmail.com>
2024-01-30 00:17:36 +0530

    file updated

--- currencyProject/src/components/input.jsx
+++ currencyProject/src/components/input.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState , useId } from "react";
 import useCurrencyHook from "../hooks/currencyHook";
 
 function Input({
@@ -13,10 +13,11 @@ function Input({
   currencyDisable = false,
   className = "",
 }) {
+  const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex  ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label htmlfor={amountInputId} className="text-black/40 mb-2 inline-block">{label}</label>
         <input
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
