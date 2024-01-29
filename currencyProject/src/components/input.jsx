f7442763279f9d6d698d26f872ec245c07f551ff
sumit021995 <kaku.saurav@gmail.com>
2024-01-30 00:18:48 +0530

    file updated

--- currencyProject/src/components/input.jsx
+++ currencyProject/src/components/input.jsx
@@ -19,6 +19,7 @@ function Input({
       <div className="w-1/2">
         <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">{label}</label>
         <input
+        id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
