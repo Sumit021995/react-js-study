e6cd22e179d3789ba95b002b3f4365e05621f1bb
sumit021995 <kaku.saurav@gmail.com>
2024-01-31 02:06:55 +0530

    file updated

--- currencyProject/src/App.jsx
+++ currencyProject/src/App.jsx
@@ -5,8 +5,8 @@ import  { Input }  from './components/index';
 
 function App() {
   const [amount, setAmount] = useState(0)
-  const [from, setFrom] = useState("USD")
-  const [to, setTo] = useState("INR")
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
   const [convertedAmount, setConvertedAmount] = useState(0)
   const currencyHook = useCurrencyHook(from)
   const currencyOptions = Object.keys(currencyHook)
