aea926bd0b24327093711f7313c45b8e4bff86c8
sumit021995 <kaku.saurav@gmail.com>
2024-02-01 01:38:14 +0530

    file updated

--- React-Router/src/components/User/User.jsx
+++ React-Router/src/components/User/User.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function User() {
+    const userId = useParams()
   return (
     <div className='bg-gray-400 text-orange-700'>
-      User :
+      User : {userId}
     </div>
   )
 }
