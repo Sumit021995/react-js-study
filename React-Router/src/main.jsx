51cbddde86e1705535d3872d42dc84b09eecd66b
sumit021995 <kaku.saurav@gmail.com>
2024-02-01 00:49:25 +0530

    File updated

--- React-Router/src/main.jsx
+++ React-Router/src/main.jsx
@@ -7,6 +7,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Layout from './Layout'
 import About from './components/About/About'
 import Home from './components/Home/Home'
+import Contact from './components/Contact/Contact'
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
     },{
       path:'about',
       element:<About/>
+    },
+    {
+      path:'contact',
+      element:<Contact/>
     }
   ]
 }
