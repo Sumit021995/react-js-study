ca9decee2fa058394c419af3eef16556f65f8c54
sumit021995 <kaku.saurav@gmail.com>
2024-02-01 01:59:57 +0530

    file updated

--- React-Router/src/components/Github/Github.jsx
+++ React-Router/src/components/Github/Github.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function Github() {
+
   
   return (
     <div className='bg-gray-500 text-white text-center text-3xl py-2'>
-      Github Followers : {'https://github.com/Sumit021995?tab=followers'}
+      Github Followers : 
     </div>
   )
 }
