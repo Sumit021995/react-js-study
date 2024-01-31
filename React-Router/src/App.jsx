5cd6dee9695b0fe22a2749af62855b88c28488d5
sumit021995 <kaku.saurav@gmail.com>
2024-02-01 00:12:22 +0530

    file updated

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
@@ -6,7 +9,9 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-600'>React Router</h1>
+      <Header/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
