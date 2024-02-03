8a7878a39482ae50df68daaf8295ac665556defb
sumit021995 <kaku.saurav@gmail.com>
2024-02-03 22:17:59 +0530

    file updated

--- miniContext/src/Context/UserContext.js
+++ miniContext/src/Context/UserContext.js
@@ -3,3 +3,5 @@ import React, { createContext } from 'react'
 const UserContext = createContext();
 // this is also an option to createContext()
 // const UserContext = React.createContext();
+
+export default UserContext();
