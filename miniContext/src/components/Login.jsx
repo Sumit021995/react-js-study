efc3d58436c5f064d12c217564a804626be779c6
sumit021995 <kaku.saurav@gmail.com>
2024-02-04 02:22:48 +0530

    file updated

--- miniContext/src/components/Login.jsx
+++ miniContext/src/components/Login.jsx
@@ -2,11 +2,13 @@ import React  , {useState , useContext }from 'react'
 import UserContext from '../Context/UserContext'
 
 function Login() {
+    
   return (
     <div>
       <h2>Login</h2>
       <input type='text' placeholder='username'/>
       <input type='text' placeholder='password'/>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   )
 }
