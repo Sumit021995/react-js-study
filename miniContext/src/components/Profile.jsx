571469b912d5982842bee79e3e6c576dffff3717
sumit021995 <kaku.saurav@gmail.com>
2024-02-04 15:13:38 +0530

    file updated

--- miniContext/src/components/Profile.jsx
+++ miniContext/src/components/Profile.jsx
@@ -5,6 +5,7 @@ function Profile() {
     const {user} = useContext(UserContext)
   
     if(!user) return <div className='mx-4 text-white my-2 text-lg'>Login Please</div>
+    if(user.username === '') return <div className='mx-4 text-white my-2 text-lg'>Login with Details</div>
     return <div className='mx-4 text-white my-2 text-lg'> Welcome {user.username}</div>
 }
 
