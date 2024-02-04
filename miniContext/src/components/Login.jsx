8397663260dd8b2ded0ed14106a5e8e9f3ca53f1
sumit021995 <kaku.saurav@gmail.com>
2024-02-04 12:29:29 +0530

    file updated

--- miniContext/src/components/Login.jsx
+++ miniContext/src/components/Login.jsx
@@ -13,11 +13,11 @@ function Login() {
     }
   return (
     <div className='mx-4 bg-gray-800'>
-      <h2 className='rounded-l mx-4 text-white text-center text-2xl'>Login</h2>
+      <h2 className='mx-4 text-white text-center text-2xl'>Login</h2>
       <div>
-      <input className='rounded-l mx-4' type='text' value = {username} onChange ={(e)=>setUsername(e.target.value)} placeholder='username'/>
+      <input className='rounded-sm mx-4' type='text' value = {username} onChange ={(e)=>setUsername(e.target.value)} placeholder='Username'/>
       <div className='my-2'>   </div>
-      <input className='rounded-l mx-4' type='text' value = {password} onChange ={(e)=>setPassword(e.target.value)} placeholder='password'/>
+      <input className='rounded-sm mx-4' type='text' value = {password} onChange ={(e)=>setPassword(e.target.value)} placeholder='Password'/>
       </div>
       <button className='rounded-xl bg-yellow-300 p-2 m-2 ' onClick={handleSubmit}>Submit</button>
     </div>
