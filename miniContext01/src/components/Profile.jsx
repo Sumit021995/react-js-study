d17c427c7b73d5daa565943f4326119ba7ebf3c0
sumit021995 <kaku.saurav@gmail.com>
2024-02-05 15:49:43 +0530

    file updated

--- miniContext01/src/components/Profile.jsx
+++ miniContext01/src/components/Profile.jsx
@@ -3,7 +3,9 @@ import UserContext from '../context/UserContext'
 
 function Profile() {
     const {user} = useContext(UserContext);
-    
+    if(!user) return <div> Login Please </div>
+    if(user.username === '' || user.username === ' ') return <div>Fill Details Before Submitting</div>
+    return <div>Welcome {user.username}</div>
 }
 
 export default Profile
