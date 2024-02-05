2d3de88d5b16930a2bbeb97af0b2c29000460d29
sumit021995 <kaku.saurav@gmail.com>
2024-02-05 15:55:37 +0530

    file updated

--- miniContext01/src/components/Login.jsx
+++ miniContext01/src/components/Login.jsx
@@ -16,6 +16,7 @@ function Login() {
     <div>
     <h1>Login</h1>
       <input type='text' value={username} placeholder='Username' onChange={(e)=>setUsername(e.target.value)}/>
+      <div>  </div>
       <input type='text' value={password} placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
       <button onClick={handleSubmit}>Submit</button>
     </div>
