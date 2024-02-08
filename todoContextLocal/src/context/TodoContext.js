00f9b73548bb0860b42b6c7f061041ce7a963a12
sumit021995 <kaku.saurav@gmail.com>
2024-02-08 22:09:30 +0530

    file updated

--- todoContextLocal/src/context/TodoContext.js
+++ todoContextLocal/src/context/TodoContext.js
@@ -1,5 +1,11 @@
 import {createContext , useContext} from 'react'
 
 export const TodoContext = createContext({
-    
+
 });
+
+export const TodoProvider = TodoContext.Provider
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
