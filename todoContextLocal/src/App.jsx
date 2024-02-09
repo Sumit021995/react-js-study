3657f232ff8c5425e1167de2d265e646dbeaec7a
sumit021995 <kaku.saurav@gmail.com>
2024-02-09 11:23:22 +0530

    file updated

--- todoContextLocal/src/App.jsx
+++ todoContextLocal/src/App.jsx
@@ -3,6 +3,19 @@ import {  } from 'react'
 
 function App() {
   
+  const addTodo = ()=>{
+    
+  }
+  const updateTodo = ()=>{
+
+  }
+  const deleteTodo = ()=>{
+
+  }
+  const toggleComplete = ()=>{
+
+  }
+  
   return (
     <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
