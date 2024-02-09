374b7ac97990e3c289e408169b83a9639730885d
sumit021995 <kaku.saurav@gmail.com>
2024-02-09 11:44:12 +0530

    file updated

--- todoContextLocal/src/App.jsx
+++ todoContextLocal/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { TodoProvider } from './context/TodoContext'
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
   }
   
   return (
+    <TodoProvider>
     <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -29,6 +31,7 @@ function App() {
                     </div>
                 </div>
             </div>
+            </TodoProvider>
   )
 }
 
