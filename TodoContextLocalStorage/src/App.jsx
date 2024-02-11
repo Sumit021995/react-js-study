7ee0827c89d408e61074c3e1458d08931e3af85d
sumit021995 <kaku.saurav@gmail.com>
2024-02-12 00:29:49 +0530

    file updated

--- TodoContextLocalStorage/src/App.jsx
+++ TodoContextLocalStorage/src/App.jsx
@@ -17,12 +17,12 @@ function App() {
   const deleteTodo = (id)=>{
     setTodosArray((prev) => prev.filter((prevTodo) => prevTodo.id !== id ))
   }
-  const checkboxTrue = (id)=>{
+  const checkboxFlip = (id)=>{
     setTodosArray((prev)=>prev.map((prevTodo)=>(prevTodo.id === id ? {...prevTodo , checkbox:!prevTodo.checkbox } : prevTodo)))
   }
 
   return (
-    <TodoProvder value={{ todosArray , addTodo , updateTodo , deleteTodo , checkboxTrue}} >
+    <TodoProvder value={{ todosArray , addTodo , updateTodo , deleteTodo , checkboxFlip}} >
     <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
