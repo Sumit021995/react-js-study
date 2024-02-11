8a253a572ec070aa7c200158ce0a7e68a34af952
sumit021995 <kaku.saurav@gmail.com>
2024-02-11 20:19:49 +0530

    file updated

--- TodoContextLocalStorage/src/App.jsx
+++ TodoContextLocalStorage/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     setTodosArray((prev)=> [{id:Date.now() , ...todoMsg},...prev])
   }
   const updateTodo = (id , todoMsg)=>{
-    setTodosArray
+    setTodosArray(()=>())
 
   }
   const deleteTodo = ()=>{
