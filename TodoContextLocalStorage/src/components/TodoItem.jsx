7a1f99b69104f2e57876f29c457b717a74ad06d1
sumit021995 <kaku.saurav@gmail.com>
2024-02-13 23:56:12 +0530

    file updated

--- TodoContextLocalStorage/src/components/TodoItem.jsx
+++ TodoContextLocalStorage/src/components/TodoItem.jsx
@@ -1,26 +1,28 @@
+import { useState } from "react";
 import { useTodo } from "../context/TodoContext";
 
 function TodoItem({ todoMsg }) {
+    const [todoMsg , setTodoMsg] = useState()
     
     const {checkboxFlip , deleteTodo , } = useTodo()
 
     return (
         <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+                todoMsg.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
             }`}
         >
             <input
                 type="checkbox"
                 className="cursor-pointer"
-                checked={todo.completed}
+                checked={todoMsg.completed}
                 onChange={checkboxFlip}
             />
             <input
                 type="text"
                 className={`border outline-none w-full bg-transparent rounded-lg ${
                     isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-                } ${todo.completed ? "line-through" : ""}`}
+                } ${todoMsg.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
                 readOnly={!isTodoEditable}
@@ -29,20 +31,20 @@ function TodoItem({ todoMsg }) {
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
                 onClick={() => {
-                    if (todo.completed) return;
+                    if (todoMsg.completed) return;
 
                     if (isTodoEditable) {
                         editTodo();
                     } else setIsTodoEditable((prev) => !prev);
                 }}
-                disabled={todo.completed}
+                disabled={todoMsg.completed}
             >
                 {isTodoEditable ? "📁" : "✏️"}
             </button>
-            {/* Delete Todo Button */}
+            {/* Delete todoMsg Button */}
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
-                onClick={() => deleteTodo(todo.id)}
+                onClick={() => deleteTodo(todoMsg.id)}
             >
                 ❌
             </button>
