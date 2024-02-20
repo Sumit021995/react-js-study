0178e8e3894a2bad33244bec979aafa8f2cd536d
sumit021995 <kaku.saurav@gmail.com>
2024-02-20 11:23:47 +0530

    file updated

--- ReduxtToolkitTodo/src/App/Store.js
+++ ReduxtToolkitTodo/src/App/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { TodoReducer } from "../Features/Todo/TodoSlice";
+import  todoReducer  from "../Features/Todo/TodoSlice";
 
 export const Store = configureStore({
-    
+    reducer: todoReducer
 })
