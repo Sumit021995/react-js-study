64237a2860b90a40ee8d3f6920a4caf32673a2a2
sumit021995 <kaku.saurav@gmail.com>
2024-02-25 01:58:49 +0530

    file updated

--- ReduxtToolkitTodo/src/main.jsx
+++ ReduxtToolkitTodo/src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import { Store } from './App/Store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={Store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
