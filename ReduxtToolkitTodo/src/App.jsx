a4e63a9033e18587f1aedd0e11a406defe0f87ce
sumit021995 <kaku.saurav@gmail.com>
2024-02-25 21:31:06 +0530

    file updated

--- ReduxtToolkitTodo/src/App.jsx
+++ ReduxtToolkitTodo/src/App.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react'
 import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
+// import { useSelector } from 'react-redux'
 
 function App() {
+  const [todos , setTodos] = useState(null)
+
+  // const todos = useSelector(state=>state.id)
   
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length>0){
+      setTodos(todos)
+    }
+  },[])
+  
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
   return (
    <>
