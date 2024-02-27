ef6b333de2eee522e7d2420158768f6f563124d1
sumit021995 <kaku.saurav@gmail.com>
2024-02-27 13:10:45 +0530

    file updated

---
+++ MyTodoReduxToolkit/src/App/Store.js
@@ -0,0 +1,6 @@
+// Here we create Store
+import { configureStore } from "@reduxjs/toolkit";
+
+export const Store = configureStore({
+    reducer:
+})
---
+++ MyTodoReduxToolkit/src/Features/TodoSlice.js
@@ -0,0 +1,30 @@
+import { createSlice , nanoid } from "@reduxjs/toolkit";
+
+
+export const initialState = {
+    todos:[{
+        id:1,
+        text:"Learn React Fully"
+    }]
+}
+
+export const TodoSlice = createSlice({
+    name:'Todo',
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const newTodo = {
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.push(newTodo)
+        },
+        removeTodo:(state,action)=>{
+            state.todos = state.todos.filter((items)=>items.id !== action.payload)
+
+        },
+        updateTodo:(state,action)=>{
+            state.todos = 
+        }
+    }
+})
