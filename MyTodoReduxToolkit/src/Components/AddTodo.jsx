0f823095b169135deed5abc858f22c4f2f60bb80
sumit021995 <kaku.saurav@gmail.com>
2024-02-27 19:00:33 +0530

    file updated

--- MyTodoReduxToolkit/src/Components/AddTodo.jsx
+++ MyTodoReduxToolkit/src/Components/AddTodo.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { addTodo } from '../Features/TodoSlice'
 
+
 function AddTodo() {
-    
+    const [input , setInput] = React.useState("")
+
+    const addTodoHandler = (e)=>{
+        e.preventDefault()
+    }
+
 
   return (
     <div>
+        <form onSubmit={addTodoHandler}>
+        <input
+        type='text'
+        placeholder='Enter Your Todo Message'
+        value={input}
+        onChange={(e)=>setInput(e.target.value)}
+
+        ></input>
+        <button
+        onClick={}
+        >Add Todo</button>
+        </form>
       
     </div>
   )
