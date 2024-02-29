28f3b0690212d652486208ce867688fdd9425862
sumit021995 <kaku.saurav@gmail.com>
2024-02-29 14:10:05 +0530

    file updated

--- MyTodoReduxToolkit/src/Components/Todos.jsx
+++ MyTodoReduxToolkit/src/Components/Todos.jsx
@@ -20,8 +20,7 @@ function Todos() {
         {todos.map((todo) => (
           <li
             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
-            key={todo.id}
-            
+            key={todo.id} 
           >
             <input 
             type='text' 
