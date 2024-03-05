26348c30f17a2dc9d6feaa411b78a59b74901e2e
sumit021995 <kaku.saurav@gmail.com>
2024-03-05 20:42:23 +0530

    file updated

--- MyTodoReduxToolkit/src/Components/Todos.jsx
+++ MyTodoReduxToolkit/src/Components/Todos.jsx
@@ -9,16 +9,17 @@ function Todos({todo}) {
   let [text , setText]=React.useState(todo.text)
     const [isEditable,setIsEditable]=React.useState(false)
     const dispatch = useDispatch()
-    // const updateTodoHandler = ()=>{
-    //   dispatch(updateTodo(text))
-    //   // setText("")
-    //   setIsEditable(false)
+    
+    const updateTodoHandler = ()=>{
+      dispatch(updateTodo(text))
+    setText("")
+      setIsEditable(false)
 
-    // }
-  //   const editTodo =()=>{
-  //     updatedTodo(todo.id , {...todo,text:todoMsg})
-  //     setIsTodoEditable(false)
-  // }
+    }
+      const editTodo =()=>{
+        updatedTodo(todo.id , {...todo,text:todoMsg})
+        setIsTodoEditable(false)
+    }
 
 
 
