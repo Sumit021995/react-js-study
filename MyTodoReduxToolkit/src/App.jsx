86cdf91e89a22a581cb4a98f74c4a0f9bebb5d0c
sumit021995 <kaku.saurav@gmail.com>
2024-03-05 21:55:38 +0530

    file updated

--- MyTodoReduxToolkit/src/App.jsx
+++ MyTodoReduxToolkit/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <div className="bg-[#172842] min-h-screen py-8 ">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-black">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2 text-white">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
@@ -19,7 +19,7 @@ function App() {
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
                         {todos.map((todo)=>(
-                          <div className='text-black' key={todo.id}>
+                          <div className='text-black w-full' key={todo.id}>
                             <Todos todo = {todo} />
                           </div>
                         ))}
--- MyTodoReduxToolkit/src/Components/AddTodo.jsx
+++ MyTodoReduxToolkit/src/Components/AddTodo.jsx
@@ -17,17 +17,17 @@ function AddTodo() {
 
   return (
     <div>
-        <form onSubmit={addTodoHandler}>
+        <form onSubmit={addTodoHandler} className='flex'>
         <input
-        className='rounded-s-sm w-2/3 '
+        className='text-white w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5'
         type='text'
         placeholder='Enter Your Todo Message'
         value={input}
         onChange={(e)=>setInput(e.target.value)}
         />
         <button type='submit'
-        className='text-white bg-red-600 rounded-e-sm '
-        > <pre> Add Todo </pre> </button>
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+        >Add Todo</button>
         </form>
       
     </div>
