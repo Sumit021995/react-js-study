59a61d37b08cee5f5c03519f2ce12232068ae494
sumit021995 <kaku.saurav@gmail.com>
2024-03-06 21:02:55 +0530

    new project updated

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
