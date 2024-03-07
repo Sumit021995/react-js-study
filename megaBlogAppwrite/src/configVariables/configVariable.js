3c48c0d90ebdbd331dcd3cec80411ccc1172775c
sumit021995 <kaku.saurav@gmail.com>
2024-03-07 12:30:02 +0530

    file updated

--- megaBlogAppwrite/src/configVariables/configVariable.js
+++ megaBlogAppwrite/src/configVariables/configVariable.js
@@ -1,9 +1,9 @@
 const configVariable = {
     appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
-    appwrite_project_id:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwrite_database_id:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwrite_collection_id:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwrite_bucket_id:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
 
