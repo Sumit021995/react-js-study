c11d6204f9184fb39016d67a6dc7cf81267fc21d
sumit021995 <kaku.saurav@gmail.com>
2024-03-07 14:17:38 +0530

    file updated

---
+++ megaBlogAppwrite/src/appwrite/auth.js
@@ -0,0 +1,19 @@
+// import { Client, Account, ID } from "appwrite";
+
+// const client = new Client()
+//     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+//     .setProject('<PROJECT_ID>');               // Your project ID
+
+// const account = new Account(client);
+
+// const promise = account.create(ID.unique(), 'email@example.com', 'password');
+
+// promise.then(function (response) {
+//     console.log(response); // Success
+// }, function (error) {
+//     console.log(error); // Failure
+// });
+
+
+// above code is by appwrite sign up 
+
